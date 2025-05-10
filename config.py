b57ce61d92afe7a00c360cc5570a26ec2d8c0dfe
vkk5880 <41853419+vkk5880@users.noreply.github.com>
2025-05-10 12:09:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class Config:
     OWNER_ID = list(map(int, os.getenv("OWNER_ID", "123456789").split()))
     
     # Channels
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1001234567890))
-    FORCE_SUB_CHANNEL = int(os.getenv("FORCE_SUB_CHANNEL", -1001234567890))
+    LOG_CHANNEL = os.getenv("LOG_CHANNEL", savecontentdiscusion)
+    FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", allfreecoursesgovtexam)
     
     # Premium
     PREMIUM_PRICES = {
